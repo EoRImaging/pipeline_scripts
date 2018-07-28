c140d8fa3d02e820b4d7c0489651aa10c2c1e5a9
Ruby Byrne <rlbyrne@uw.edu>
2018-07-27 18:19:46 -0700

    code to run eppsilon on one obs at a time

---
+++ bash_scripts/eppsilon_single_obs_job_aws.sh
@@ -0,0 +1,102 @@
+#! /bin/bash
+
+#############################################################################
+# Runs one eppsilon on observation at a time in grid engine on AWS
+#############################################################################
+
+
+echo JOBID ${JOB_ID}
+echo TASKID ${SGE_TASK_ID}
+echo OBSID ${obs_id}
+echo "JOB START TIME" `date +"%Y-%m-%d_%H:%M:%S"`
+myip="$(dig +short myip.opendns.com @resolver1.opendns.com)"
+echo PUBLIC IP ${myip}
+
+#set defaults
+if [ -z ${nslots} ]; then
+    nslots=10
+fi
+if [ -z ${outdir} ]; then
+    outdir=/FHD_output
+fi
+if [ -z ${s3_path} ]; then
+    s3_path=s3://mwatest/diffuse_survey
+fi
+if [ -z ${version} ]; then
+    >&2 echo "ERROR: no version provided"
+    exit 1
+fi
+if [ -z ${obs_id} ]; then
+    >&2 echo "ERROR: no obs ID provided"
+    exit 1
+fi
+if [ -z ${image_window_name} ]; then
+    image_window_name=''
+fi
+if [ -z ${refresh_ps} ]; then
+    refresh_ps=0
+fi
+
+#strip the last / if present in output directory filepath
+outdir=${outdir%/}
+s3_path=${s3_path%/}
+
+#create output directory with full permissions
+if [ -d "$outdir" ]; then
+    sudo chmod -R 777 $outdir
+else
+    sudo mkdir -m 777 $outdir
+fi
+
+# Check that the FHD outputs exist on S3
+fhd_exists=$(aws s3 ls ${s3_path}/fhd_${version})
+if [ -z "$fhd_exists" ]; then
+    >&2 echo "ERROR: FHD outputs not found"
+    echo "Job Failed"
+    exit 1
+fi
+
+# Copy FHD job from S3
+aws s3 cp ${s3_path}/fhd_${version}/ ${outdir}/fhd_${version}/ --recursive \
+--exclude "*" --include "*${obs_id}*" --quiet
+
+# Run FHD
+idl -IDL_DEVICE ps -IDL_CPU_TPOOL_NTHREADS $nslots -e aws_ps_single_obs_job -args \
+$obs_id $outdir $version $image_window_name $refresh_ps || :
+
+if [ $? -eq 0 ]
+then
+    echo "Eppsilon Job Finished"
+    error_mode=0
+else
+    echo "Job Failed"
+    error_mode=1
+fi
+
+# Move outputs to S3
+i=1  #initialize counter
+aws s3 mv ${outdir}/fhd_${version}/ps/ ${s3_path}/fhd_${version}/ps/ \
+--recursive --exclude "*" --include "*${obs_id}*" --quiet
+while [ $? -ne 0 ] && [ $i -lt 10 ]; do
+    let "i += 1"  #increment counter
+    >&2 echo "Moving eppsilon outputs to S3 failed. Retrying (attempt $i)."
+    aws s3 mv ${outdir}/fhd_${version}/ps/ ${s3_path}/fhd_${version}/ps/ \
+    --recursive --exclude "*" --include "*${obs_id}*" --quiet
+done
+
+# Remove outputs from the instance
+sudo rm -r ${outdir}/fhd_${version}
+
+echo "JOB END TIME" `date +"%Y-%m-%d_%H:%M:%S"`
+
+# Copy gridengine stdout to S3
+aws s3 cp ~/grid_out/eppsilon_single_obs_job_aws.sh.o${JOB_ID} \
+${s3_path}/fhd_${version}/grid_out/eppsilon_single_obs_job_aws.sh.o${JOB_ID}_${myip}.txt \
+--quiet
+
+# Copy gridengine stderr to S3
+aws s3 cp ~/grid_out/eppsilon_single_obs_job_aws.sh.e${JOB_ID} \
+${s3_path}/fhd_${version}/grid_out/eppsilon_single_obs_job_aws.sh.e${JOB_ID}_${myip}.txt \
+--quiet
+
+exit $error_mode
---
+++ eppsilon_IDL_wrappers/aws_ps_single_obs_job.pro
@@ -0,0 +1,33 @@
+pro aws_ps_single_obs_job
+  ;wrapper for ps_wrapper to take in shell parameters from a grid engine bash script
+
+  ;***Read-in typical parameters
+  compile_opt strictarr
+  args = Command_Line_Args(count=nargs)
+  obs_id=args[0]
+  outdir=args[1]
+  fhd_version=args[2]
+  image_window_name=args[3]
+  refresh_ps=args[4]
+
+  if image_window_name eq '' then begin
+    image_window_name = 0
+    image_window_frac_size = 0
+  endif else begin
+    if image_window_name eq 'Blackman-Harris' then begin
+      image_window_frac_size = 0.5
+    endif else begin
+      print, 'Invalid image_window_name. Returning.'
+      return
+    endelse
+  endelse
+
+  refresh_ps = fix(refresh_ps)
+  if (refresh_ps ne 0) and (refresh_ps ne 1) then begin
+    print, 'Parameter refresh_ps must be 0 or 1. Returning.'
+    return
+  fi
+
+  ps_wrapper, outdir+'/fhd_'+fhd_version, obs_id, /png, image_window_name=image_window_name, image_window_frac_size=image_window_frac_size, refresh_ps=refresh_ps
+
+end
