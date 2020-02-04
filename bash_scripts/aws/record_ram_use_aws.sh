c8e0b172dd20b4acdf7d37da71465d7d84527e8c
Ruby Byrne <rlbyrne@uw.edu>
2020-02-03 21:59:19 -0800

    report used RAM, not total

--- bash_scripts/aws/record_ram_use_aws.sh
+++ bash_scripts/aws/record_ram_use_aws.sh
@@ -8,6 +8,6 @@ myip=$4
 
 while true; do
     date >> ${outdir}/fhd_${version}/metadata/ram_usage_${JOB_ID}_${myip}.txt
-    awk '/Mem:/ {print $2}' <(free -m) >> ${outdir}/fhd_${version}/metadata/ram_usage_${JOB_ID}_${myip}.txt
+    awk '/Mem:/ {print $3}' <(free -m) >> ${outdir}/fhd_${version}/metadata/ram_usage_${JOB_ID}_${myip}.txt
     sleep 60 # Record RAM usage every 60 seconds
 done
