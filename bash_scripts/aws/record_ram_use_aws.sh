204df4904684439240060773bcd62e604434c165
Ruby Byrne <rlbyrne@uw.edu>
2020-02-06 15:38:55 -0800

    bug fix

--- bash_scripts/aws/record_ram_use_aws.sh
+++ bash_scripts/aws/record_ram_use_aws.sh
@@ -7,8 +7,9 @@ version=$3
 JOB_ID=$4
 myip=$5
 
+echo 'RAM usage (MB):' > ${outdir}/fhd_${version}/metadata/${obs_id}_ram_usage_${JOB_ID}_${myip}.txt
 while true; do
     date >> ${outdir}/fhd_${version}/metadata/${obs_id}_ram_usage_${JOB_ID}_${myip}.txt
-    awk '/Mem:/ {print $3}' <(free -m) >> ${outdir}/fhd_${version}/metadata/ram_usage_${JOB_ID}_${myip}.txt
+    awk '/Mem:/ {print $3}' <(free -m) >> ${outdir}/fhd_${version}/metadata/${obs_id}_ram_usage_${JOB_ID}_${myip}.txt
     sleep 60 # Record RAM usage every 60 seconds
 done
