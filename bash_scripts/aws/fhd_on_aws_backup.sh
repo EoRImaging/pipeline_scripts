53982f4bf88d5b691b85d9275986930a3f75835b
Ruby Byrne <rlbyrne@uw.edu>
2020-02-25 15:41:23 -0800

    fix AWS spot termination backup bug

--- bash_scripts/aws/fhd_on_aws_backup.sh
+++ bash_scripts/aws/fhd_on_aws_backup.sh
@@ -15,14 +15,10 @@ while true; do
         >&2 echo "Spot instance termination. Shutting down."
 
         # Copy gridengine stdout to S3
-        aws s3 cp ~/grid_out/fhd_job_aws.sh.o${JOB_ID} \
-        ${s3_path}/fhd_${version}/grid_out/\
-        fhd_job_aws.sh.o${JOB_ID}_${myip}.txt
+        aws s3 cp ~/grid_out/fhd_job_aws.sh.o${JOB_ID} ${s3_path}/fhd_${version}/grid_out/fhd_job_aws.sh.o${JOB_ID}_${myip}.txt
 
         # Copy gridengine stderr to S3
-        aws s3 cp ~/grid_out/fhd_job_aws.sh.e${JOB_ID} \
-        ${s3_path}/fhd_${version}/grid_out/\
-        fhd_job_aws.sh.e${JOB_ID}_${myip}.txt
+        aws s3 cp ~/grid_out/fhd_job_aws.sh.e${JOB_ID} ${s3_path}/fhd_${version}/grid_out/fhd_job_aws.sh.e${JOB_ID}_${myip}.txt
 
         # Copy FHD outputs to S3
         j=1  #initialize counter
