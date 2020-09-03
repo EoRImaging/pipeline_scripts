65c4475837b9e2664fcf2aece9d9aba180358871
Ruby Byrne <rlbyrne@uw.edu>
2020-09-03 16:45:15 -0700

    change run fhd command to slurm

--- bash_scripts/aws/run_fhd_aws.sh
+++ bash_scripts/aws/run_fhd_aws.sh
@@ -240,5 +240,5 @@ fi
 
 for obs_id in "${good_obs_list[@]}"
 do
-    qsub -V -b y -cwd -v nslots=${nslots},outdir=${outdir},version=${version},s3_path=${s3_path},obs_id=${obs_id},versions_script=${versions_script},uvfits_s3_loc=${uvfits_s3_loc},metafits_s3_loc=${metafits_s3_loc},run_ps=${run_ps},ps_uvf_input=${ps_uvf_input},input_vis=${input_vis},input_eor=${input_eor},extra_vis=${extra_vis},cal_transfer=${cal_transfer},model_uv_transfer=${model_uv_transfer} -e ${logdir} -o ${logdir} -pe smp ${nslots} -sync y fhd_job_aws.sh &
+    sbatch -n ${nslots} --export=nslots=${nslots},outdir=${outdir},version=${version},s3_path=${s3_path},obs_id=${obs_id},versions_script=${versions_script},uvfits_s3_loc=${uvfits_s3_loc},metafits_s3_loc=${metafits_s3_loc},run_ps=${run_ps},ps_uvf_input=${ps_uvf_input},input_vis=${input_vis},input_eor=${input_eor},extra_vis=${extra_vis},cal_transfer=${cal_transfer},model_uv_transfer=${model_uv_transfer} -e ${logdir} -o ${logdir} fhd_job_aws.sh &
 done
