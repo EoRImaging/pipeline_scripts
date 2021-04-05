86c0ada9555c52e722ee5ad415798fb94d45f585
pyxieloustar <pyxiestar@gmail.com>
2021-04-05 09:57:13 -0700

    add wrappers for integration/eppsilon jobs; update calls file

---
+++ bash_scripts/azure/eppsilon_job_az.sh
@@ -0,0 +1,179 @@
+#! /bin/bash
+
+# Batch script for slurm job. Second level program for
+# running integration on azure machines. First level program is run_eppsilon_az.sh
+# Adapted from eppsilon_job_aws.sh
+
+#inputs needed: file_path_cubes, obs_list_path, obs_list_array, version, nslots
+#inputs optional: cube_type, pol, evenodd
+
+# If cube_type is unset, do a power spectrum job. Otherwise, do a cube job.
+
+echo JOBID ${SLURM_ARRAY_JOB_ID}
+if [ ! -z ${cube_type} ]; then
+    echo TASKID ${SLURM_ARRAY_TASK_ID}
+fi
+echo VERSION ${version}
+echo "JOB START TIME" `date +"%Y-%m-%d_%H:%M:%S"`
+myip="$(dig +short myip.opendns.com @resolver1.opendns.com)"
+echo PUBLIC IP ${myip}
+
+# If doing cube job, use task id to get index for even/odd and pol arrays
+# These tasks might wait on corresponding tasks in integration_job_az.sh
+# This code must be the same in both scripts for the tasks to correspond correctly
+if [ ! -z ${cube_type} ]; then
+    evenodd_arr=('even' 'odd')
+    pol_arr=($pols)
+    n_pols=${#pol_arr[@]}
+    evenodd_ind=$(((${SLURM_ARRAY_TASK_ID}-1)/${n_pols}))
+    pol_ind=$(((${SLURM_ARRAY_TASK_ID}-1)%${n_pols}))
+    pol=(${pol_arr[$pol_ind]})
+    evenodd=(${evenodd_arr[$evenodd_ind]})
+    echo Processing cube: ${pol} ${evenodd} ${cube_type}
+fi
+
+# Set single_obs default
+if [ -z ${single_obs} ]; then
+    single_obs=0
+fi
+
+# echo keywords
+echo Using file_path_cubes: $file_path_cubes
+echo Using version: $version
+echo Using single_obs: $single_obs
+
+# Create a string of arguements to pass into az_ps_job given the input
+#   into this script
+input_folder=/mnt/scratch/$version/
+if [[ -z ${cube_type} ]] && [[ -z ${pol} ]] && [[ -z ${evenodd} ]]; then
+    # create arg string for ps job
+	arg_string="${input_folder} ${version}"
+else
+    if [[ ! -z ${cube_type} ]] && [[ ! -z ${pol} ]] && [[ ! -z ${evenodd} ]]; then
+        # create arg string for cube job
+	    arg_string="${input_folder} ${version} ${cube_type} ${pol,,} ${evenodd}"
+    else
+        echo "Need to specify cube_type, pol, and evenodd altogether"
+        exit 1
+    fi
+fi
+
+#create Healpix download location with full permissions
+if [ -d ${version}/Healpix ]; then
+    sudo chmod -R 777 ${version}/Healpix
+else
+    sudo mkdir -m 777 ${version}
+    sudo mkdir -m 777 ${version}/Healpix
+fi
+#create PS download location with full permissions
+if [ -d ${version}/ps ]; then
+    sudo chmod -R 777 ${version}/ps
+    if [ -d ${version}/ps/data ]; then
+        sudo chmod -R 777 ${version}/ps/data
+    else
+        sudo mkdir -m 777 ${version}/ps/data
+    fi
+    if [ -d ps/data/uvf_cubes ]; then
+        sudo chmod -R 777 ${version}/ps/data/uvf_cubes
+    else
+        sudo mkdir -m 777 ${version}/ps/data/uvf_cubes
+    fi
+else
+    sudo mkdir -m 777 ${version}
+    sudo mkdir -m 777 ${version}/ps
+    sudo mkdir -m 777 ${version}/ps/data
+    sudo mkdir -m 777 ${version}/ps/data/uvf_cubes
+fi
+
+if [ $single_obs -eq 1 ]; then
+    cube_prefix=${version}
+	echo Working on a single obsid. Using cube prefix ${cube_prefix}.
+else
+	cube_prefix="Combined_obs_${version}"
+	echo Working on combined obsids. Using cube prefix ${cube_prefix}.
+fi 
+
+# If doing a cube job, look for integration cube locally.
+# If not found, download from azure
+if [ ! -z ${cube_type} ]; then
+    if [ ! -f "${version}/Healpix/${cube_prefix}_${evenodd}_cube${pol^^}.sav" ]; then
+        az storage copy -s ${file_path_cubes}/Healpix/${cube_prefix}_${evenodd}_cube${pol^^}.sav \
+        -d ${version}/Healpix/${cube_prefix}_${evenodd}_cube${pol^^}.sav
+    fi
+    # Check that file downloaded
+    if [ ! -f "${version}/Healpix/${cube_prefix}_${evenodd}_cube${pol^^}.sav" ]; then
+        >&2 echo "Integration cube ${cube_prefix}_${evenodd}_cube${pol^^}.sav not found"
+        exit 1
+    fi
+# If doing a power spectra job, need all the weights, model, and dirty cubes.
+# If not found locally, download from azure
+else
+    # should have n_cubes * n_pols * n_evenodd cubes
+    n_epps_cubes = $((n_cubes*n_pol*2))
+    file_num=$(ls ${version}/ps/data/uvf_cubes/${cube_prefix}* | wc -li)
+    echo "file_num is $file_num"
+    if [ $file_num -ne $n_epps_cubes ]; then
+        # Download from azure
+        az storage copy -s ${file_path_cubes}/ps/data/uvf_cubes -d ${version}/ps/data --recursive
+    fi
+    # Check download
+    file_num=$(ls ${version}/ps/data/uvf_cubes/${cube_prefix}* | wc -li)
+    if [ $file_num -ne $n_epps_cubes ]; then
+        >&2 echo "Unable to make power spectra. Missing uvf cubes."
+        echo "Job Failed"
+        exit 1
+    fi
+fi
+
+# Run eppsilon
+echo "arg_string is $arg_string"
+idl -IDL_DEVICE ps -IDL_CPU_TPOOL_NTHREADS $nslots -e az_ps_job -args $arg_string || :
+
+if [ $? -eq 0 ]
+then
+    echo "Eppsilon Job Finished"
+    error_mode=0
+else
+    echo "Eppsilon Job Failed"
+    error_mode=1
+fi
+
+# Move eppsilon outputs to az
+if [ -z ${cube_type} ]; then
+    i=1  #initialize counter
+    az storage copy -s ${version}/ps -d ${file_path_cubes} --recursive
+    while [ $? -ne 0 ] && [ $i -lt 10 ]; do
+        let "i += 1"  #increment counter
+        >&2 echo "Moving eppsilon outputs to az failed. Retrying (attempt $i)."
+        az storage copy -s ps -d ${file_path_cubes} --recursive
+    done
+else
+    i=1  #initialize counter
+    az storage copy -s ${version}/ps/data/uvf_cubes -d ${file_path_cubes}/ps/data --recursive
+    while [ $? -ne 0 ] && [ $i -lt 10 ]; do
+        let "i += 1"  #increment counter
+        >&2 echo "Moving eppsilon outputs to az failed. Retrying (attempt $i)."
+        az storage copy -s ps/data/uvf_cubes -d ${file_path_cubes}/ps/data --recursive
+    done
+fi
+
+echo "JOB END TIME" `date +"%Y-%m-%d_%H:%M:%S"`
+
+# Move logs to az
+if [ ! -z ${cube_type} ]; then
+    # Copy stdout to S3
+    az storage copy -s ~/logs/${version}_eppsilon_cube_job_az.sh.o${SLURM_ARRAY_JOB_ID}.${SLURM_ARRAY_TASK_ID} \
+    -d ${file_path_cubes}/ps/logs/${version}_eppsilon_cube_job_az.sh.o${SLURM_ARRAY_JOB_ID}.${SLURM_ARRAY_TASK_ID}_${myip}.txt
+    # Copy stderr to S3
+    az storage copy -s ~/logs/${version}_eppsilon_cube_job_az.sh.e${SLURM_ARRAY_JOB_ID}.${SLURM_ARRAY_TASK_ID} \
+    -d ${file_path_cubes}/ps/logs/${version}_eppsilon_cube_job_az.sh.e${SLURM_ARRAY_JOB_ID}.${SLURM_ARRAY_TASK_ID}_${myip}.txt
+else
+    # Copy stdout to S3
+    az storage copy -s ~/logs/${version}_eppsilon_ps_job_az.sh.o${SLURM_ARRAY_JOB_ID} \
+    -d ${file_path_cubes}/ps/logs/${version}_eppsilon_ps_job_az.sh.o${SLURM_ARRAY_JOB_ID}_${myip}.txt
+    # Copy stderr to S3
+    az storage copy -s ~/logs/${version}_eppsilon_ps_job_az.sh.e${SLURM_ARRAY_JOB_ID} \
+    -d ${file_path_cubes}/ps/logs/${version}_eppsilon_ps_job_az.sh.e${SLURM_ARRAY_JOB_ID}_${myip}.txt
+fi
+
+exit $error_mode
---
+++ bash_scripts/azure/integration_job_az.sh
@@ -0,0 +1,107 @@
+#!/bin/bash
+
+# Batch script for slurm job. Second level program for
+# running integration on azure machines. First level program is run_eppsilon_az.sh
+# Adapted from integration_job_aws.sh
+
+echo JOBID ${SLURM_ARRAY_JOB_ID}
+echo TASKID ${SLURM_ARRAY_TASK_ID}
+echo "JOB START TIME" `date +"%Y-%m-%d_%H:%M:%S"`
+myip="$(dig +short myip.opendns.com @resolver1.opendns.com)"
+echo PUBLIC IP ${myip}
+
+# Use task id to get index for even/odd and pol arrays
+# Corresponding tasks in eppsilon_job_az.sh might wait on these tasks
+# This code must be the same in both scripts for the tasks to correspond correctly
+evenodd_arr=('even' 'odd')
+pol_arr=($pols)
+n_pols=${#pol_arr[@]}
+evenodd_ind=$(((${SLURM_ARRAY_TASK_ID}-1)/${n_pols}))
+pol_ind=$(((${SLURM_ARRAY_TASK_ID}-1)%${n_pols}))
+pol=(${pol_arr[$pol_ind]})
+evenodd=(${evenodd_arr[$evenodd_ind]})
+echo Processing cube: ${pol} ${evenodd}
+
+# echo keywords
+echo Using file_path_cubes: $file_path_cubes
+echo Using version: $version
+
+#create Healpix download location with full permissions
+if [ -d ${version}/Healpix ]; then
+    sudo chmod -R 777 ${version}/Healpix
+else
+    sudo mkdir -m 777 ${version}
+    sudo mkdir -m 777 ${version}/Healpix
+fi
+
+# set integrated cube file name
+save_file_evenoddpol=${version}/Healpix/Combined_obs_${version}_${evenodd}_cube${pol^^}.sav
+
+# check if Healpix cubes exist locally; if not, try to download
+unset exit_flag
+for int_cube in $(cat $int_list_path); do
+    if [ ! -f "${version}/Healpix/${int_cube}_${evenodd}_cube${pol^^}.sav" ]; then
+        az storage copy -s ${file_path_cubes}/Healpix/${int_cube}_${evenodd}_cube${pol^^}.sav \
+        -d ${version}/Healpix/${int_cube}_${evenodd}_cube${pol^^}.sav
+    fi
+    # check if cube downloaded
+    if [ ! -f "${version}/Healpix/${int_cube}_${evenodd}_cube${pol^^}.sav" ]; then
+        >&2 echo "ERROR: HEALPix file not downloaded: ${int_cube}_${evenodd}_cube${pol^^}.sav"
+        exit_flag=1
+    else
+        echo Downloaded ${int_cube}_${evenodd}_cube${pol^^}.sav
+    fi
+done
+
+# Error if any cubes did not download
+if [ ! -z ${exit_flag} ]; then
+    exit 1
+fi
+echo All cubes on instance
+
+#Create a name for the downloaded cube file based off of inputs
+evenoddpol_file_paths=${version}/Healpix/${version}_${evenodd}${pol^^}_list.txt
+
+# Fill the downloaded cube file with paths to cubes to integrate
+for int_cube in $(cat $int_list_path); do
+    cube_path=${version}/Healpix/${int_cube}_${evenodd}_cube${pol^^}.sav
+    echo $cube_path >> $evenoddpol_file_paths
+done
+
+# Run the integration IDL script
+idl -IDL_DEVICE ps -IDL_CPU_TPOOL_NTHREADS $nslots -e integrate_healpix_cubes -args "$evenoddpol_file_paths" "$save_file_evenoddpol" || :
+
+if [ $? -eq 0 ]
+then
+    echo "Integration Job Finished"
+    error_mode=0
+else
+    echo "Job Failed"
+    error_mode=1
+fi
+
+# Move integration output file to az
+i=1  #initialize counter
+az storage copy -s ${save_file_evenoddpol} -d ${file_path_cubes}/${save_file_evenoddpol}
+while [ $? -ne 0 ] && [ $i -lt 10 ]; do
+    let "i += 1"  #increment counter
+    >&2 echo "Moving FHD outputs to az failed. Retrying (attempt $i)."
+    az storage copy -s ${save_file_evenoddpol} -d ${file_path_cubes}/${save_file_evenoddpol}
+done
+
+echo "JOB END TIME" `date +"%Y-%m-%d_%H:%M:%S"`
+
+# Copy stdout to S3
+az storage copy -s ~/logs/${version}_integration_job_az.sh.o${SLURM_ARRAY_JOB_ID}.${SLURM_ARRAY_TASK_ID} \
+-d ${file_path_cubes}/Healpix/logs/${version}_integration_job_az.sh.o${SLURM_ARRAY_JOB_ID}.${SLURM_ARRAY_TASK_ID}_${myip}.txt
+
+# Copy stderr to S3
+az storage copy -s ~/logs/${version}_integration_job_az.sh.e${SLURM_ARRAY_JOB_ID}.${SLURM_ARRAY_TASK_ID} \
+-d ${file_path_cubes}/Healpix/logs/${version}_integration_job_az.sh.e${SLURM_ARRAY_JOB_ID}.${SLURM_ARRAY_TASK_ID}_${myip}.txt
+
+# Remove integration cubes from the instance
+for int_cube in $(cat $int_list_path); do
+    sudo rm ${version}/Healpix/${int_cube}_${evenodd}_cube${pol^^}.sav
+done
+
+exit $error_mode
