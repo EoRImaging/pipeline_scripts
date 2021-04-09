3c25f188e44ba989e4c263b669900e90ce6c928f
Mike Wilensky <mjw768@uw.edu>
2021-04-09 08:48:44 -0700

    do a second login after FHD finishes

--- bash_scripts/azure/fhd_job_az.sh
+++ bash_scripts/azure/fhd_job_az.sh
@@ -195,6 +195,9 @@ else
     error_mode=1
 fi
 
+# sign into azure again
+az login --identity
+
 # Copy FHD outputs to az
 i=1  # initialize counter
 # az storage copy -s ${outdir}/fhd_${version} -d ${az_path}/fhd_${version} --recursive --include-pattern "*${obs_id}*"
