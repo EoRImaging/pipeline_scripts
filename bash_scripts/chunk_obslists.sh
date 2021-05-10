2a904179babe1f1e1c35b657dc85dc242f4492d0
Mike Wilensky <mjw768@uw.edu>
2021-05-10 12:00:24 -0700

    submit non chunked jobs too

--- bash_scripts/chunk_obslists.sh
+++ bash_scripts/chunk_obslists.sh
@@ -17,18 +17,23 @@ do
       for shape in narrow all_TV all_RFI_types streak
       do
         filename="2014_iono_cut_z_band_${z_band}_${shape}_${pointing}${clean_tag}.txt"
-	num_lines=$(wc -l $filename | cut -f 1 -d ' ')
-	echo $num_lines
-	if [ -f $filename ] && [ $num_lines -gt 20 ]; then
-          bn=$(basename -s .txt ${filename})
-          chunk_pre="${bn}_chunk_"
-          split -l 20 -a 1 $filename "${chunk_pre}"
-          chunks="$(ls ${chunk_pre}?)"
-          for obsfile in $chunks
-          do
-            bash run_eppsilon_az.sh -d ${file_path_cubes} -f ${obsfile} -v ${obsfile} -n 8 -i 1 -q htc -o 1
-          done
-          echo $chunks >> ${bn}_chunk_list.txt
+      	num_lines=$(wc -l $filename | cut -f 1 -d ' ')
+      	echo $num_lines
+      	if [ -f $filename ]; then
+          if [ $num_lines -gt 20 ]; then
+            bn=$(basename -s .txt ${filename})
+            chunk_pre="${bn}_chunk_"
+            split -l 20 -a 1 $filename "${chunk_pre}"
+            chunks="$(ls ${chunk_pre}?)"
+            for obsfile in $chunks
+              do
+                bash run_eppsilon_az.sh -d ${file_path_cubes} -f ${obsfile} -v ${obsfile} -n 8 -i 1 -q htc -o 1 -c 0 -p 0
+              done
+              echo $chunks >> ${bn}_chunk_list.txt
+          else
+            version="2014_iono_cut_z_band_${z_band}_${shape}_${pointing}${clean_tag}"
+            bash run_eppsilon_az.sh -d ${file_path_cubes} -f ${filename} -v ${version} -n 8 -i 1 -q htc -o 1
+          fi
         fi
       done
     done
